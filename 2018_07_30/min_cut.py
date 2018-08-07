dcbc67042df3e333b247f2a308a2ef0691a47f3a
Michael Anderson <mganderson87@gmail.com>
2018-08-06 20:43:18 -0400

    Revise min cut algorithm

--- 2018_07_30/min_cut.py
+++ 2018_07_30/min_cut.py
@@ -4,70 +4,26 @@ import random
 
 
 def min_cut(adj_list):
-    def get_edges(adj_list):
-        edges = []
-        for node, neighbors in adj_list.items():
-            for neighbor in neighbors:
-                edges.append((node, neighbor))
 
-        # Detect self loops:
-        for edge in edges:
-            if edge[0] == edge[1]:
-                # print(edges)
-                print(edge)
-                raise ValueError("Deteted self loop")
+    edges = []
+    for node, neighbors in adj_list.items():
+        for neighbor in neighbors:
+            edges.append((node, neighbor))
+
+    def contract_edges(adj_list, edges, merged_away, merged_into):
+        del adj_list[merged_away]
+        edges = [
+            (edge[0] if edge[0] != merged_away else merged_into,
+             edge[1] if edge[1] != merged_away else merged_into)
+             for edge in edges
+        ]
+        # Remove self-loops
+        edges = [edge for edge in edges if edge[0] != edge[1]]
         return edges
 
-    def contract(adj_list, edge):
-        merged_away, merged_into = edge[0], edge[1]
-        merged_away_edges = adj_list[merged_away]
-        adj_list[merged_into] += merged_away_edges
-        del adj_list[merged_away]
-        for node, neighbors in adj_list.items():
-            adj_list[node] = [
-                (neighbor if neighbor != merged_away else merged_into)
-                for neighbor in neighbors
-            ]
-        for node, neighbors in adj_list.items():
-            adj_list[node] = [
-                neighbor
-                for neighbor in neighbors
-                if neighbor != node
-            ]
-
-        # neighbors_to_update = adj_list[merged_away]
-        # for neighbor in neighbors_to_update:
-        #     adj_list[neighbor] = [
-        #         node if node != merged_away else merged_into
-        #         for node in adj_list[neighbor]
-        #     ]
-        #     # Delete self-loops
-        #     adj_list[neighbor] = [
-        #         node
-        #         for node in adj_list[neighbor]
-        #         if node != neighbor
-        #     ]
-        # del adj_list[merged_away]
-
-        # for neighbor_node in adj_list[merged_away]:
-        #     adj_list[neighbor_node] = [
-        #         (neighbor if neighbor != merged_away else merged_into)
-        #         for neighbor in adj_list[neighbor_nodeu]
-        #     ]
-        #     adj_list[neighbor_node] = [
-        #         neighbor
-        #         for neighbor in adj_list[neighbor_node]
-        #         if neighbor != neighbor_node  # remove self-loops
-        #     ]
-        # del(adj_list[merged_away])
-
-    edges = get_edges(adj_list)
-    iterations = 0
     while len(adj_list) > 2:
         edge = random.choice(edges)
-        contract(adj_list, edge)
-        edges = get_edges(adj_list)
-        iterations += 1
+        edges = contract_edges(adj_list, edges, edge[0], edge[1])
     return len(edges) // 2
 
 
@@ -78,8 +34,8 @@ def main():
             node_and_neighbors = [int(num) for num in line.split()]
             adj_list[node_and_neighbors[0]] = node_and_neighbors[1:]
 
-    n = len(adj_list)
-    trials = (n ** 2) * math.log(n)
+    n = len(adj_list)  # Number of nodes in the graphs
+    trials = (n ** 2) * math.log(n)  # Trials needed to get failure risk of 1/n
     min_cut_so_far = None
     for i in range(math.ceil(trials)):
         result = min_cut(copy.deepcopy(adj_list))
