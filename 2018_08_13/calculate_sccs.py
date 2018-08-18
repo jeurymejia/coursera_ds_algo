7f8b0025a0bd366a29041a0a87fd83ae23051c08
Michael Anderson <mganderson87@gmail.com>
2018-08-18 16:32:18 -0400

    Fix reverse_edges(), add input parsing function for SCC assignment

--- 2018_08_13/calculate_sccs.py
+++ 2018_08_13/calculate_sccs.py
@@ -3,8 +3,26 @@
 from itertools import dropwhile
 
 
+def build_graph(filepath):
+    graph = {}
+    with open(filepath, 'r') as file:
+        for edge in file:
+            head, tail = edge.split()
+            try:
+                graph[head].append(tail)
+            except KeyError:
+                graph[head] = [tail]
+    return graph
+
+
 def reverse_edges(graph):
-    """Reverse the edges of a graph in place."""
+    """Reverse the edges of a graph in place.
+
+    Args:
+        graph: An adjacency list representation of a directed graph
+    Returns:
+        None (graph is modified in-place)
+    """
     # Add None to the end of each list of edges to act as sentinel value
     for node in graph:
         try:
@@ -12,15 +30,18 @@ def reverse_edges(graph):
         except AttributeError:
             graph[node] = [None]
     # Add each new edge after the None sentinel
+    new_key_values = {}
     for node, edge_heads in graph.items():
-        new_key_values = {}
         for head in edge_heads:
             if head is None:
                 break
             try:
                 graph[head].append(node)
             except KeyError:
-                new_key_values[head] = [None, node]
+                try:
+                    new_key_values[head].append(node)
+                except KeyError:
+                    new_key_values[head] = [None, node]
     # Add any new key-values to original adjacency list
     graph.update(new_key_values)
     # Remove all edges before the None sentinel, as well as the sentinel
